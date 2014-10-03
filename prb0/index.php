681175ecb670dde2c74140cf4290fde1c252f38c
Pablo D. Sánchez <pdsanchez.contacto@gmail.com>
2014-10-03 19:48:35 +0200

    miscellaneous

---
+++ prb0/index.php
@@ -0,0 +1,17 @@
+<?php
+$v1 = new stdClass();
+#echo $v1;
+
+if ($v1 == true) {
+  echo " - ok true";
+}
+
+echo "<br>";
+
+$v2 = new stdClass();
+#echo $v2;
+
+if ($v2 == false) {
+  echo " - ok false";
+}
+?>
